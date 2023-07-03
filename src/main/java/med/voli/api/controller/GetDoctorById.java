0500a1f98726fb4a77c024c607c8e4cc5254bb25
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-07-03 17:42:51 -0300

    created route to return one doctor following rest

---
+++ src/main/java/med/voli/api/controller/GetDoctorById.java
@@ -0,0 +1,26 @@
+package med.voli.api.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import med.voli.api.doctor.DataDetailDoctor;
+import med.voli.api.doctor.IDoctorRepository;
+
+@RestController
+@RequestMapping("/get-doctor/{id}")
+public class GetDoctorById {
+    
+    @Autowired
+    private IDoctorRepository repo;
+
+    @GetMapping
+    public ResponseEntity<DataDetailDoctor> detail(@PathVariable Long id) {
+        var doctor = repo.getReferenceById(id);
+
+        return ResponseEntity.ok(new DataDetailDoctor(doctor));
+    }
+}
