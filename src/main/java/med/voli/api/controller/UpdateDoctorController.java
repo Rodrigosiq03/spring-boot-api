36343a6b4de453559ddb3c76533a8efe3d91acfb
Rodrigo <rodrigo.dsiqueira1@gmail.com>
2023-07-03 17:38:23 -0300

    fixed path variable error

--- src/main/java/med/voli/api/controller/UpdateDoctorController.java
+++ src/main/java/med/voli/api/controller/UpdateDoctorController.java
@@ -1,6 +1,8 @@
 package med.voli.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,20 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voli.api.doctor.DataDetailDoctor;
 import med.voli.api.doctor.DataUpdateDoctor;
 import med.voli.api.doctor.IDoctorRepository;
 
 @RestController
-@RequestMapping("/update-doctor")
+@RequestMapping("/update-doctor/{id}")
 public class UpdateDoctorController {
-
-    class ResponseTransfer {
-        public String message;
-
-        public ResponseTransfer(String message) {
-            this.message = message;
-        }
-    }
 
     @Autowired
     private IDoctorRepository repo;
@@ -30,12 +25,11 @@ public class UpdateDoctorController {
     @PutMapping
     @Transactional
     @ResponseBody
-    public ResponseTransfer updateDoctor(@RequestBody @Valid DataUpdateDoctor data) {
-
-        var doctor = repo.findById(data.id()).get();
+    public ResponseEntity updateDoctor(@PathVariable long id, @RequestBody @Valid DataUpdateDoctor data) {
+        var doctor = repo.getReferenceById(id);
         doctor.updateInfo(data);
 
-        return new ResponseTransfer("Doctor updated successfully!");
+        return ResponseEntity.ok(new DataDetailDoctor(doctor));
 
     }
     
